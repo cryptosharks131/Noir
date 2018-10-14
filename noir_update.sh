d7e362447a6166052364acee3603e901569b1292
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2018-10-14 11:28:27 -0500

    Update noir_update.sh

--- noir_update.sh
+++ noir_update.sh
@@ -20,7 +20,7 @@ function update_node() {
   COIN_ZIP=$(echo $COIN_REPO | awk -F'/' '{print $NF}')
   tar xvf $COIN_ZIP --strip 1 >/dev/null 2>&1
   compile_error
-  cp bin/noir{d,-cli} /usr/local/bin
+  cp noir{d,-cli} /usr/local/bin
   compile_error
   strip $COIN_DAEMON $COIN_CLI
   cd - >/dev/null 2>&1
