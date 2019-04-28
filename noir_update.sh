02677c85ae9d0e6432448a67fbeab113e2c42272
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2019-04-28 07:46:05 -0500

    Add 18.04 support

--- noir_update.sh
+++ noir_update.sh
@@ -40,9 +40,9 @@ fi
 }
 
 function checks() {
-if [[ $(lsb_release -d) != *16.04* ]]; then
-  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
-  exit 1
+if [[ $(lsb_release -d) != *16.04* ]] && [[ $(lsb_release -d) != *18.04* ]]; then
+   echo -e "${RED}You are not running Ubuntu 16.04 or 18.04. Installation is cancelled.${NC}"
+   exit 1
 fi
 
 if [[ $EUID -ne 0 ]]; then
