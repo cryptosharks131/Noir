4b1d186e0b0af88c8cf99a6f24d4c66a273daf07
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2019-04-29 07:15:49 -0500

    Revert 18.04

--- noir_update.sh
+++ noir_update.sh
@@ -40,10 +40,11 @@ fi
 }
 
 function checks() {
-if [[ $(lsb_release -d) != *16.04* ]] && [[ $(lsb_release -d) != *18.04* ]]; then
-   echo -e "${RED}You are not running Ubuntu 16.04 or 18.04. Installation is cancelled.${NC}"
-   exit 1
+if [[ $(lsb_release -d) != *16.04* ]]; then
+    echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
+    exit 1
 fi
+}
 
 if [[ $EUID -ne 0 ]]; then
    echo -e "${RED}$0 must be run as root.${NC}"
