f1155455d412b6ffb29a3169578ff44dd239e99a
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2019-06-22 09:30:58 -0500

    v2.0.0.0

--- noir_install.sh
+++ noir_install.sh
@@ -7,11 +7,11 @@ CONFIGFOLDER='/root/.noir'
 COIN_DAEMON='/usr/local/bin/noird'
 COIN_CLI='/usr/local/bin/noir-cli'
 COIN_REPO='https://github.com/cryptosharks131/Noir/releases/download/v2.0.0.0/noir.tar.gz'
-COIN_REPO18='https://github.com/cryptosharks131/Noir/releases/download/v1.0.1.1/noir18.tar.gz'
+COIN_REPO18='https://github.com/cryptosharks131/Noir/releases/download/v2.0.0.0/noir18.tar.gz'
 COIN_NAME='Noir'
 COIN_RPC=8822
 COIN_PORT=8255
-COIN_BS='https://github.com/cryptosharks131/Noir/releases/download/v1.0.1.1/bootstrap.tar.gz'
+COIN_BS='https://github.com/cryptosharks131/Noir/releases/download/v2.0.0.0/bootstrap.tar.gz'
 
 NODEIP=$(curl -s4 icanhazip.com)
 
